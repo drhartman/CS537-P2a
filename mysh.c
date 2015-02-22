9350bc94f2760dffe8ddbe3b5cf24b7ccc6f0a68
drhartman <drhartman9@gmail.com>
2015-02-22 14:08:41 -0600

    added ls, pwd, and running prog functionality
    still need to add text overwrite, piping, and tee

--- mysh.c
+++ mysh.c
@@ -3,6 +3,11 @@
 #include <string.h>
 #include <stdbool.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include "mysh.h"
 #define _GNU_SOURCE
 
 // ssize_t getline(char **lineptr, size_t *n, FILE *stream);
@@ -10,39 +15,20 @@
 
 //   printf("here \n");
 
-int
-runcmd(char* args[], int numArgs)
-{
-	// exit command
-	if (((strcmp("exit", args[0]) == 0)) || 
-	     (strcmp("exit\n", args[0]) == 0))
-	{
-		// return a 1 and set exit = true
-		return 1;
-	}
-	
-	// change directory
-	
-		
-
-	return 0;
-}
-
-
 int main (void)
 {
 	char* command;
 	size_t comLength = (1024*sizeof(char));
 	ssize_t readNum;
 	char* args[1024];
-	char* delim = " ";
+	char* delim = " \n";
 	bool exit = false;
 	
 	// error messaging
 	//const char * const sys_errlist[10];
      //int sys_nerr;
      int errno;
-     char* errorMes;
+     //char* errorMes;
 	
 	command = (char*) malloc(1024*sizeof(char));
 
@@ -57,8 +43,8 @@ int main (void)
 		
 		if (readNum == -1)
 		{
-			errorMes = "getline";
-			perror(errorMes);
+			//errorMes = "getline";
+			perror("getline");
 		}
 		
 		// parse commands and add all arguments to an arary
@@ -72,21 +58,82 @@ int main (void)
 			args[argsNum] = strtok(NULL, delim);
 		}
 		
-		//TODO: account for either '\n' or ' ' as last arg
-		//if (args[argsNum-1] == 
+		// separate args printout
+		printf("\n\n");
 		
-		//run built-in command
-		if (argsNum == 1)
+		/* Built-in Commands */
+		
+		// exit command
+		//TODO: does not account for >1 args
+		if (((strcmp("exit", args[0]) == 0)) || 
+		     (strcmp("exit\n", args[0]) == 0))
 		{
-			if(runcmd(args, argsNum) == 1)
+			exit = true;
+		}
+	
+		// change directory
+		else if ((strcmp("cd", args[0]) == 0))
+		{
+			if (args[1] == NULL)
 			{
-				exit = true;
+				char* home = getenv("HOME");
+				chdir(home);
+			}
+
+			// change to directory desired
+			else if(chdir(args[1]) == -1)
+			{
+				perror("cd");
+			}
+		}
+		
+		// print current working directory
+		else if (strcmp("pwd", args[0]) == 0)
+		{
+			char* cwd = NULL;
+			cwd = get_current_dir_name();
+			printf("%s\n", cwd);
+		}
+		
+			
+		
+		// if not a command above, is it a prog??
+		else {
+				
+			// Running Programs
+			// returns -1 if error
+			// fork to split 
+			int pid;
+			pid = fork();
+		
+			// if pid == 0, this is the child
+			if (pid == 0)
+			{
+				//printf("child\n");
+				//char* prog = getenv(args[0]);
+				//printf("%s\n", prog);
+				//prog = get_current_dir_name();
+				execvp(args[0], args);
+				
+				//should not return after exec
+				printf("should not get here, ERROR");
+				kill(pid, SIGKILL);
+			}
+			// this is the parent 
+			else 
+			{
+				//printf("parent\n");
+				// wait until child process is terminated
+				if (wait(NULL) == -1)
+				{
+					perror("wait");
+				}
+			
 			}
 			
-		}
-			
-			
-			
+		} //end else
+		
+		
 	} // end while
 
 	free(command);
